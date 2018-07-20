f57ef036c627c3a5c468f1aab669a24eb98b7d12
Sebastian Monzel <mail@sebastianmonzel.de>
2018-07-20 20:53:14 +0200

    updated versions, init ocr

--- src/main/java/de/sebastianmonzel/textextractor/OcrTextExtractor.java
+++ src/main/java/de/sebastianmonzel/textextractor/OcrTextExtractor.java
@@ -1,5 +1,10 @@
 package de.sebastianmonzel.textextractor;
 
+import com.asprise.ocr.Ocr;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
 import java.io.*;
 
 public class OcrTextExtractor extends AbstractTextExtractor {
@@ -31,6 +36,48 @@ public class OcrTextExtractor extends AbstractTextExtractor {
 
     @Override
     public AbstractTextExtractor extractText() {
+
+        // TODO alternativen evaluieren
+
+        //-->http://tess4j.sourceforge.net/ - http://tess4j.sourceforge.net/codesample.html
+        //http://www.ocrserver.at/
+        //https://cloud.google.com/vision/docs/
+        //http://stackoverflow.com/questions/1813881/java-ocr-implementation
+
+
+
+        /*Ocr.setUp();
+        for (String s:
+        Ocr.listSupportedLanguages()) {
+            System.out.println(s);
+        };*/
+
+        //Ocr ocr = new Ocr();
+        //ocr.startEngine("deu",Ocr.SPEED_FAST);
+        File textFile = new File("src/test/resources/textinimagetoscanviaocr.png");
+
+        File[] textfiles = {textFile};
+        //String recognizedtext = ocr.recognize(textfiles, Ocr.RECOGNIZE_TYPE_TEXT, Ocr.OUTPUT_FORMAT_PLAINTEXT);
+        //System.out.println(recognizedtext);
+
+        File barcodeFile = new File("src/test/resources/getCode.png");
+        File[] barcodefiles = {barcodeFile};
+        //String recognizedbarcode = ocr.recognize(barcodefiles, Ocr.RECOGNIZE_TYPE_BARCODE, Ocr.OUTPUT_FORMAT_PLAINTEXT);
+        //System.out.println(recognizedbarcode);
+
+
+        //ocr.stopEngine();
+
+
+        ITesseract instance = new Tesseract();
+        try {
+            String result = instance.doOCR(textFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+
         return null;
     }
+
 }
--- src/main/java/de/sebastianmonzel/textextractor/PdfTextExtractor.java
+++ src/main/java/de/sebastianmonzel/textextractor/PdfTextExtractor.java
@@ -1,7 +1,7 @@
 package de.sebastianmonzel.textextractor;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.util.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 import java.io.*;
 
